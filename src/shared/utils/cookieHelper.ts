4c322b47b363da4f756cc819b794b1eed7c66f1d
Mohammed Rimshan <rimshanshanu55@gmail.com>
2025-07-15 08:21:24 +0530

    Cookie changed

--- src/shared/utils/cookieHelper.ts
+++ src/shared/utils/cookieHelper.ts
@@ -2,45 +2,49 @@ import { Response } from "express";
 import { config } from "../config";
 
 export const setAuthCookies = (
-	res: Response,
-	accessToken: string,
-	refreshToken: string,
-	accessTokenName: string,
-	refreshTokenName: string
+  res: Response,
+  accessToken: string,
+  refreshToken: string,
+  accessTokenName: string,
+  refreshTokenName: string
 ) => {
-	const isProduction = config.server.NODE_ENV === "production";
+  const isProduction = config.server.NODE_ENV === "production";
 
-	res.cookie(accessTokenName, accessToken, {
-		httpOnly: true,
-		secure: isProduction,
-		sameSite: "strict",
-	});
+  res.cookie(accessTokenName, accessToken, {
+    httpOnly: true,
+    secure: isProduction, 
+    sameSite: isProduction ? "none" : "lax", 
+    path: "/", 
+    maxAge: 15 * 60 * 1000, 
+  });
 
-	res.cookie(refreshTokenName, refreshToken, {
-		httpOnly: true,
-		secure: isProduction,
-		sameSite: "strict",
-	});
+  res.cookie(refreshTokenName, refreshToken, {
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "lax",
+    path: "/",
+    maxAge: 7 * 24 * 60 * 60 * 1000, 
+  });
 };
 
 export const updateCookieWithAccessToken = (
-	res: Response,
-	accessToken: string,
-	accessTokenName: string
+  res: Response,
+  accessToken: string,
+  accessTokenName: string
 ) => {
-	const isProduction = config.server.NODE_ENV === "production";
-	res.cookie(accessTokenName, accessToken, {
-		httpOnly: true,
-		secure: isProduction,
-		sameSite: "strict",
-	});
+  const isProduction = config.server.NODE_ENV === "production";
+  res.cookie(accessTokenName, accessToken, {
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: "strict",
+  });
 };
 
 export const clearAuthCookies = (
-	res: Response,
-	accessTokenName: string,
-	refreshTokenName: string
+  res: Response,
+  accessTokenName: string,
+  refreshTokenName: string
 ) => {
-	res.clearCookie(accessTokenName);
-	res.clearCookie(refreshTokenName);
+  res.clearCookie(accessTokenName);
+  res.clearCookie(refreshTokenName);
 };
